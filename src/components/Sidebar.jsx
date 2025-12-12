19eb8e0cc4ee4c5a54ce192e808798ad7a6e9528
gungun <boragungun7@gmail.com>
2025-12-12 17:31:48 +0530

    rightsidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -24,7 +24,7 @@ const Sidebar = ({selectedUser,setSelectedUser}) => {
                 </div>
                 { /*  search bar  */}
             <div className='bg-[#282142] rounded-full flex items-center gap-2 py-3 px-4 mt-5'>
-           <img src={assets.search_icon} alt="search" classname='w-3' />
+           <img src={assets.search_icon} alt="search" className='w-3 ' />
            <input type="text" className='bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1' placeholder='Search User...' />
         </div>
 
